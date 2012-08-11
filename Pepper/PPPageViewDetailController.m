02e7db0b9c719d6966f6b09e2889500a3b39f77a
Torin Nguyen <torinnguyen@gmail.com>
2012-08-11 20:17:27 +0800

    remove debug code

--- Pepper/PPPageViewDetailController.m
+++ Pepper/PPPageViewDetailController.m
@@ -10,49 +10,8 @@
 @implementation PPPageViewDetailController
 @synthesize tag;
 
-- (void)viewDidLoad
-{
-  [super viewDidLoad];
-  
-  /* For debugging
-  self.view.backgroundColor = [UIColor redColor];
-
-  UIButton *button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-  button1.frame = CGRectMake(0, 0, 100, 32);
-  button1.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
-  [self.view addSubview:button1];
-  
-  UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-  button2.frame = CGRectMake(self.view.bounds.size.width-100, 0, 100, 32);
-  button2.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin;
-  [self.view addSubview:button2];
-  
-  UIButton *button3 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-  button3.frame = CGRectMake(0, self.view.bounds.size.height-32, 100, 32);
-  button3.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-  [self.view addSubview:button3];
-  
-  UIButton *button4 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-  button4.frame = CGRectMake(self.view.bounds.size.width-100, self.view.bounds.size.height-32, 100, 32);
-  button4.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
-  [self.view addSubview:button4];
-   */
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-  [super viewWillAppear:animated];
-  
-  /*
-  for (UIButton *button in self.view.subviews)
-    if ([button isKindOfClass:[UIButton class]])
-      [button setTitle:[NSString stringWithFormat:@"%d", self.view.tag] forState:UIControlStateNormal];
-   */
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-  return YES;
-}
+/*
+ This class is just an empty UIViewController to carry PPPageViewDetailWrapper for implementing UIPageViewController
+ */
 
 @end
